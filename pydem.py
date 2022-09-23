9e5ef75ced543070bd9a68915e0c8f26fb0662d0
kugelrund <6236137+kugelrund@users.noreply.github.com>
2022-09-23 17:47:55 +0200

    Also remove superhealth flag in spawnparams

--- pydem.py
+++ pydem.py
@@ -88,7 +88,10 @@ def smooth(demo):
 
 def next_spawnparams(stats: format.ClientStats) -> format.ClientStats:
     new_stats = copy.deepcopy(stats)
-    new_stats.items = stats.items & ~(stats.items & (ItemFlags.KEY1|ItemFlags.KEY2|ItemFlags.INVISIBILITY|ItemFlags.INVULNERABILITY|ItemFlags.SUIT|ItemFlags.QUAD))
+    new_stats.items = stats.items & ~(stats.items & (
+        ItemFlags.SUPERHEALTH|ItemFlags.KEY1|ItemFlags.KEY2|
+        ItemFlags.INVISIBILITY|ItemFlags.INVULNERABILITY|ItemFlags.SUIT|
+        ItemFlags.QUAD))
     new_stats.health = min(max(stats.health, 50), 100)
     new_stats.shells = max(stats.shells, 25)
     return new_stats
