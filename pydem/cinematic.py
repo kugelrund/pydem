c1fbfc1d99c40407f7bb6af6a4e188764f1fc8f0
kugelrund <6236137+kugelrund@users.noreply.github.com>
2024-01-28 22:50:58 +0100

    Avoid spamming fade for repeating timestamps

--- pydem/cinematic.py
+++ pydem/cinematic.py
@@ -8,17 +8,22 @@ def get_time_messages(block):
 
 
 def fade(demo, time_start, duration, backwards):
+    time_previous = None
     for b in (reversed(demo.blocks) if backwards else demo.blocks):
         time_messages = get_time_messages(b)
         if not time_messages:
             continue
         time_current = numpy.average([m.time for m in time_messages])
+        if time_current == time_previous:
+            # do not repeat cshift command if same time
+            continue
         time_elapsed = time_start - time_current if backwards else time_current - time_start
         opacity = min(1.0, 1.0 - (time_elapsed / duration))
         opacity_byte = int(round(255 * opacity))
         if opacity_byte <= 0:
             break
         b.messages.append(messages.StuffTextMessage(f"v_cshift 0 0 0 {opacity_byte}".encode()))
+        time_previous = time_current
 
 
 def fadein(demo, duration):
